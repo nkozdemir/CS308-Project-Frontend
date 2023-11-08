5e9413d6bfa0e175c1da5cbc39112785f8671093
lamasta97 <71608426+lamasta97@users.noreply.github.com>
2023-11-08 12:59:10 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,80 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Login'; // Import the Login component
+import Home from './Home'; // Import the Home component
+import AddSong from './Addsong'; // Import the AddSong component
 
-
-function HomePage() {
-  const [songName, setSongName] = useState('');
-  const [performer, setPerformer] = useState('');
-  const [album, setAlbum] = useState('');
-  const [userRating, setUserRating] = useState('');
-  const [songList, setSongList] = useState([]);
-
-  const handleAddSong = () => {
-    // Create a new song object with user input and add it to the list
-    const newSong = {
-      name: songName,
-      performer,
-      album,
-      rating: userRating,
-    };
-
-    setSongList([...songList, newSong]);
-
-    // Clear the input fields
-    setSongName('');
-    setPerformer('');
-    setAlbum('');
-    setUserRating('');
-  };
-
+const App = () => {
   return (
-    <div>
-      <h1>Music Analysis and Recommendation</h1>
-      <h2>Add a Song</h2>
-      <div>
-        <label>Song Name:</label>
-        <input
-          type="text"
-          value={songName}
-          onChange={(e) => setSongName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Performer(s):</label>
-        <input
-          type="text"
-          value={performer}
-          onChange={(e) => setPerformer(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Album:</label>
-        <input
-          type="text"
-          value={album}
-          onChange={(e) => setAlbum(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>User Rating:</label>
-        <input
-          type="number"
-          value={userRating}
-          onChange={(e) => setUserRating(e.target.value)}
-        />
-      </div>
-      <button onClick={handleAddSong}>Add Song</button>
-
-      <h2>Song List</h2>
-      <ul>
-        {songList.map((song, index) => (
-          <li key={index}>
-            <strong>{song.name}</strong> by {song.performer} from {song.album} (Rating: {song.rating})
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} /> {/* Login page as the initial landing page */}
+        <Route path="/home" element={<Home />} /> {/* Home page route */}
+        <Route path="/add-song" element={<AddSong />} /> {/* Route for AddSong */}
+      </Routes>
+    </Router>
   );
-}
+};
 
-export default HomePage;
+export default App;
