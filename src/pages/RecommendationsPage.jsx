fd023ba17e6ddfc9ff85b0d1e3ad880d57a62993
k44n-max <73305630+nkozdemir@users.noreply.github.com>
2024-01-03 21:32:59 +0300

    Add html2canvas library and update file upload headers

--- src/pages/RecommendationsPage.jsx
+++ src/pages/RecommendationsPage.jsx
@@ -1,19 +1,23 @@
 import SongRecommendations from "../components/recommendations/SongRecommendations";
+/*
 import SongRecommendationsFriends from "../components/recommendations/SongRecommendationsFriends";
 import SongRecommendationsLatest from "../components/recommendations/SongRecommendationsLatest";
 import SongRecommendationsPerf from "../components/recommendations/SongRecommendationsPerf";
+*/
 
 const RecommendationsPage = () => {
     return (
         <div className="my-20 p-4">
             <h1 className="font-bold flex items-start justify-start text-3xl mb-4">Recommended Songs For You</h1>
             <SongRecommendations />
+            {/*
             <div className="divider my-8"></div>
             <SongRecommendationsLatest />
             <div className="divider my-8"></div>
             <SongRecommendationsPerf />
             <div className="divider my-8"></div>
             <SongRecommendationsFriends />
+            */}
         </div>
     );
 };
